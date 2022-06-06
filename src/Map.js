89966c395cc999809a836adcc8b443b16e45c9bb
ahnjungeun <616a65@gmail.com>
2022-06-07 01:56:47 +0900

    버튼으로 마커 보이기/숨기기까지 함. 근데 이제 api를 다르게 해서 띄우는 걸 해야 함. 즉, 다 갈아엎어야 한다는 얘기~

--- src/Map.js
+++ src/Map.js
@@ -1,31 +1,33 @@
 import React, { useState, useCallback, useRef,useEffect } from 'react'
 import { GoogleMap, useLoadScript, Marker, Polyline, InfoWindow, Data } from '@react-google-maps/api';
-// import { formatRelative } from "data-fns"
 import axios from "axios"
 
-const libraries = ["places"];
+
 const mapContainerStyle = {
   width: "100vw",
   height: "100vh",
 }
 const center = {
-  lat: 37.6584,
-  lng: 126.8320
+  lat: 37.560876699999795, lng: 126.8210406999998,
 }
 const options = {
   disableDefaultUI: true,
   zoomControl: true
 }
-const points = [
-  { lat: 37.65, lng: 126.80, name: "test1" },
-  { lat: 37.6586, lng: 126.8578, name: "test2" }
-]
 
+const serverPoints = []
 function getAllMarkers() {
   return axios.get('http://3.39.217.105:3000/markers')
 }
 
-const serverPoints = []
+
+const dummy = [
+  { lat: 37.6584, lng: 126.8320, name: "주차장 1", category: "parking", },
+  { lat: 37.65, lng: 126.83, name: "주차장 2", category: "parking", },
+  { lat: 37.67, lng: 126.84, name: "길 1", category: "road", },
+  { lat: 37.64, lng: 126.82, name: "길 2", category: "road", },
+]
+
 
 
 export default function Map() {
@@ -34,19 +36,9 @@ export default function Map() {
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
   
-  const [markers, setMarkers] = useState([]);
+  const [markers, setMarkers] = useState([]); // 클릭한 곳의 마커좌표 저장
   const [selected, setSelected] = useState(null); // 어떤 마커가 선택됐는지
-
-  useEffect(() => {
-    var res = getAllMarkers();
-    res.then(response => response.data.result.map(x =>
-      setMarkers(current => [...current, {
-        lat: x.coordinates.lat,
-        lng: x.coordinates.lng,
-        name: x.name
-      }])
-    ));
-  }, [])
+  const [parkingVisible, setParkingVisible] = useState(false);
   
   useEffect(() => { 
     var res = getAllMarkers();
@@ -55,17 +47,20 @@ export default function Map() {
         {
           lat: x.coordinates.lat,
           lng: x.coordinates.lon,
-          name: x.name
+          name: x.name,
+          category: x.category,
         }
       )
     ))
+    .catch(new Error())
+    console.log(res)
   }, [])
 
   const onMapClick = useCallback((event) => {
     setMarkers(current => [...current,{
       lat: event.latLng.lat(),
       lng: event.latLng.lng(),
-      // 키값을 어떻게 넣지
+      //time: new Data(),// 키값을 어떻게 넣지
     }]);
   }, []);
 
@@ -78,6 +73,9 @@ export default function Map() {
   if (!isLoaded) return "Loading Maps";
 
   return <div>
+    <button
+      onClick={() => {setParkingVisible(!parkingVisible);}}
+    >주차창 보기</button>
     <GoogleMap
       mapContainerStyle={mapContainerStyle}
       zoom={13}
@@ -88,7 +86,7 @@ export default function Map() {
     >
       
       {console.log(serverPoints)}
-      {serverPoints.map(marker =>
+      {/* {serverPoints.map(marker =>
         <Marker
           key={parseFloat(marker.lat)}
           position={{ lat: parseFloat(marker.lat), lng: parseFloat(marker.lng) }}
@@ -96,9 +94,22 @@ export default function Map() {
             setSelected(marker);
           }}
         />
+      )} */}
+
+      {/* {console.log(parkingVisible)} */}
+      {serverPoints.map(marker =>
+          marker.category === 'parking' && !parkingVisible ? null : <Marker
+          key={parseFloat(marker.lat)}
+          position={{ lat: parseFloat(marker.lat), lng: parseFloat(marker.lng) }}
+          onClick={() => {
+            setSelected(marker);// 마커 좌표 저장
+          }}
+        />
       )}
 
-      {!markers ? null : markers.map(marker =>
+
+      {/* 임시주석 얘 기능하는 것 지우면 안 됨 */}
+      {markers.map(marker =>
         <Marker
           key={parseFloat(marker.lat)}
           position={{ lat: parseFloat(marker.lat), lng: parseFloat(marker.lng) }}
@@ -115,7 +126,8 @@ export default function Map() {
             setSelected(null); //토글링 안 하면 인포윈도 제일 첨에만 뜸
           }}>
           <div>
-            <h2>{ selected.name }</h2>
+            <h2>{selected.name}</h2>
+            <h3>{selected.category}</h3>
             <p>{selected.lat} {selected.lng}</p>
           </div>
         </InfoWindow>)}
